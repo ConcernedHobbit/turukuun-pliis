c9677e7765d67ea3b7c021e603366ceec69d78fd
Eetu Raunio <eetu.raunio@helsinki.fi>
2021-11-16 12:09:18 +0200

    finish week 2 warm up

---
+++ laskarit/viikko2/maksukortti/tasks.py
@@ -0,0 +1,13 @@
+from invoke import task
+
+@task
+def test(ctx):
+    ctx.run("pytest src")
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
