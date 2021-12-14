9fada6b12ab9230b2d32c2aea3090be1f7a02270
Eetu Raunio <eetu.raunio@helsinki.fi>
2021-12-14 23:23:35 +0200

    feat: person docstrings

--- src/logic/person.py
+++ src/logic/person.py
@@ -12,11 +12,27 @@ BASE_YEAR = 2020
 
 # TODO: Seperate sprite logic into an encapsulating class to abstract it away from game logic
 class Person(pygame.sprite.Sprite):
+    """
+    A class representing an in-game person
+    """
     def __init__(self,
                  name: Optional[str] = 'Matti Meikäläinen', # TODO: Random generation
                  age: Optional[int] = randrange(12, 80),
                  height: Optional[int] = randrange(150, 220),
                  ssid: Optional[SSID] = None) -> None:
+        """
+        The constructor for class Person.
+
+        Args:
+            name (str, optional): The name of the person.
+                Defaults to 'Matti Meikäläinen'.
+            age (int, optional): The age of the person.
+                Defaults to a value between [12, 80[.
+            height (int, optional): The height of the person.
+                Defaults to a value between [150, 220[.
+            ssid (SSID, optional): The SSID of the person.
+                Randomly generates one based on age if not provided.
+        """
         pygame.sprite.Sprite.__init__(self)
 
         self.name = name
@@ -42,17 +58,36 @@ class Person(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
     def set_image(self, image: pygame.Surface):
+        """
+        (will be refactored)
+        Sets the image for this Person's pygame surface
+
+        Args:
+            image (pygame.Surface): The image to be set
+        """
         self.image = image
         self.rect = self.image.get_rect()
 
     def generate_details_surface(self, font: pygame.freetype.Font,
                                  color: Optional[tuple] = (0, 0, 0),
-                                 size: Optional[int] = 42) -> pygame.Surface:
-        name, _ = font.render(f'{self.name}', fgcolor=color, size=size)
+                                 font_size: Optional[int] = 42) -> pygame.Surface:
+        """
+        (will be refactored)
+        Generates a surface containing the person's details.
+
+        Args:
+            font (pygame.freetype.Font)
+            color (tuple, optional): Defaults to (0, 0, 0).
+            font_size (int, optional): Defaults to 42.
+
+        Returns:
+            pygame.Surface: A surface containing the details
+        """
+        name, _ = font.render(f'{self.name}', fgcolor=color, size=font_size)
         birthday, _ = font.render(
-            f'Born {self.ssid.birthday}', fgcolor=color, size=size)
-        ssid, _ = font.render(f'{self.ssid}', fgcolor=color, size=size)
-        height, _ = font.render(f'{self.height} cm', fgcolor=color, size=size)
+            f'Born {self.ssid.birthday}', fgcolor=color, size=font_size)
+        ssid, _ = font.render(f'{self.ssid}', fgcolor=color, size=font_size)
+        height, _ = font.render(f'{self.height} cm', fgcolor=color, size=font_size)
 
         padding = 10
         lines = [name, birthday, ssid, height]
