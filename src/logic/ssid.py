04a10969baa11d4e05ee2ea900113a61114ecd4e
Eetu Raunio <eetu.raunio@helsinki.fi>
2021-12-14 23:31:33 +0200

    feat: ssid docstring

--- src/logic/ssid.py
+++ src/logic/ssid.py
@@ -3,9 +3,26 @@ from random import randrange
 from typing import Optional
 
 class SSID:
+    """
+    A class representing a Finnish personal identity code (henkilötunnus)
+
+    More information: https://dvv.fi/en/personal-identity-code
+    """
     def __init__(self,
                  birthday: date,
                  individual_number: Optional[int] = randrange(2, 900)) -> None:
+        """
+        Constructor for SSID
+
+        Args:
+            birthday (date): The birthday this SSID will correspond to
+            individual_number (int, optional): The individual number of this SSID.
+                Defaults to randrange(2, 900).
+
+        Raises:
+            ValueError: If birthday is outside the range of [1800, 2099]
+            ValueError: If the individual number is outside the range of [2, 899]
+        """
         if 1800 <= birthday.year < 2100:
             self.birthday = birthday
         else:
@@ -19,26 +36,48 @@ class SSID:
 
     @property
     def _date(self) -> str:
+        """
+        Returns:
+            str: Birthday in format ddmmyy
+        """
         # date.strftime cannot process years before 1900, so we need to manually format...
         return f'{self.birthday.day:02d}{self.birthday.month:02d}{str(self.birthday.year)[2:]}'
 
     @property
     def _padded_individual_number(self) -> str:
+        """
+        Returns:
+            str: The individual number left-padded with zeroes
+        """
         return str(self.individual_number).zfill(3)
 
     @property
     def _niner(self) -> str:
+        """
+        Returns:
+            str: The birthday in format ddmmyy followed by the zero-padded individual number
+        """
         return f'{self._date}{self._padded_individual_number}'
 
     @property
     def _checkdigit(self) -> str:
-        # https://dvv.fi/en/personal-identity-code
+        """
+        Returns the checkdigit of this SSID.
+        Based on a mod 31 operation on the niner property.
+        More information: https://dvv.fi/en/personal-identity-code
+        Returns:
+            str: The checkdigit for this SSID
+        """
         mod = int(self._niner) % 31
         return ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
                 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y'][mod]
 
     @property
     def _seperator(self) -> str:
+        """
+        Returns:
+            str: The century-defined seperator for this SSID
+        """
         century = self.birthday.year // 100
 
         if century == 18:
@@ -52,6 +91,15 @@ class SSID:
         return 'A'
 
     def matches_birthyear(self, year: int) -> bool:
+        """
+        Checks if this SSID matches a given birthyear
+
+        Args:
+            year (int): The year to check against
+
+        Returns:
+            bool: If this SSID matches the birthyear
+        """
         return self.birthday.year == year
 
     def __repr__(self) -> str:
