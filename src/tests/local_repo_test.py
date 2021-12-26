dc05001c63382bc8dcde011a05e9fdbe22218b8e
Eetu Raunio <eetu.raunio@helsinki.fi>
2021-12-26 09:23:02 +0200

    feat: bump up local repo test coverage

--- src/tests/local_repo_test.py
+++ src/tests/local_repo_test.py
@@ -25,12 +25,28 @@ class LocalRepoTest(unittest.TestCase):
 
     def test_saving(self):
         string_list = ['alpha', 'beta', 'gamma']
-        self.local.save_string_list(string_list, 'gamma')
+        return_value = self.local.save_string_list(string_list, 'gamma')
+
+        self.assertTrue(return_value)
 
         self.assertTrue(os.path.exists(os.path.join('test_data', 'gamma')))
         with open(os.path.join('test_data', 'gamma'), 'r', encoding = 'utf-8') as file:
             self.assertEqual(file.read(), 'alpha,beta,gamma')
 
-    def test_loading(self):
+    def test_reading(self):
         string_list = self.local.read_string_list('omega')
         self.assertListEqual(string_list, ['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+
+    def test_can_not_read_nonexistant_file(self):
+        return_value = self.local.read_string_list('kappa')
+        self.assertIsNone(return_value)
+
+    def test_can_not_save_comma_strings(self):
+        string_list = ['test,', 'ca,se', 's']
+        return_value = self.local.save_string_list(string_list, 'pi')
+        self.assertFalse(return_value)
+
+    def test_can_not_save_empty_name(self):
+        string_list = ['alpha', 'beta', 'gamma']
+        return_value = self.local.save_string_list(string_list, '')
+        self.assertFalse(return_value)
